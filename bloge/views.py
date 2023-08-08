d19cc77deca2303ffa1efb31779d09aa01fae0cd
ogabek <quvvatullayev@gmail.com>
2023-08-08 16:39:47 +0500

    create post list

--- bloge/views.py
+++ bloge/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.request import Request
+from rest_framework.response import Response
+from .serializers import PostSerializer, UserSerializer
+from .models import Post
+from django.contrib.auth.models import User
 
-# Create your views here.
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
