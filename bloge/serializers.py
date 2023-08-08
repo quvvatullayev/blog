b7a4de1375ba63063a93b24a985ff2f4e66218a2
ogabek <quvvatullayev@gmail.com>
2023-08-08 17:10:43 +0500

    create post list and fix

--- bloge/serializers.py
+++ bloge/serializers.py
@@ -1,14 +1,15 @@
-from rest_framework.serializers import Serializer
+from rest_framework.serializers import Serializer, ModelSerializer
 from bloge.models import Post
 from django.contrib.auth.models import User
 
-class UserSerializer(Serializer):
+class UserSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'email')
+        fields = ['id', 'username', 'email']
 
-class PostSerializer(Serializer):
+class PostSerializer(ModelSerializer):
+    author = UserSerializer()
     class Meta:
         model = Post
-        fields = ('title', 'author', 'body', 'created_at', 'updated_at')
+        fields = ['id','title', 'author', 'body', 'created_at', 'updated_at']
         depth = 1
--- bloge/views.py
+++ bloge/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from .serializers import PostSerializer, UserSerializer
 from .models import Post
 from django.contrib.auth.models import User
+from rest_framework.views import APIView
 
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
